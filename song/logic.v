69f8cefff6beb2166989c0d5159a64cc7cc76699
Junxiao Shi <git@mail1.yoursunny.com>
2018-08-26 19:53:55 -0400

    song: switch to sequential logic
    93 LUTs

--- song/logic.v
+++ song/logic.v
@@ -6,70 +6,72 @@ output buzzer;
 input clk;   // 2.08MHz
 input tempo; // 32Hz
 
-reg [11:1] state;
-wire [17:1] div;
+reg [9:1] state;
+reg [17:1] div;
 wire enable;
 
 defparam w.width = 12;
 wave w(.buzzer(buzzer), .clk(clk), .enable(enable), .div(div), .volume(1'b1));
 
-always @(posedge tempo) begin
-  state <= state + 1;
-end
+wire [6:0] index;
+assign index = state[9:3];
 
-wire [8:0] index; // 8Hz
-assign index = state[11:3];
-assign div =
-  (index <=   1) ? M6 :
-  (index <=   3) ? M6 :
-  (index <=   7) ? M7 :
-  (index <=   9) ? M6 :
-  (index <=  11) ? M6 :
-  (index <=  15) ? M7 :
-  (index <=  17) ? M6 :
-  (index <=  19) ? M7 :
-  (index <=  21) ? H1 :
-  (index <=  23) ? M7 :
-  (index <=  25) ? M6 :
-  (index <=  26) ? M7 :
-  (index <=  27) ? M6 :
-  (index <=  31) ? M4 :
-  (index <=  33) ? M3 :
-  (index <=  35) ? M1 :
-  (index <=  37) ? M3 :
-  (index <=  39) ? M4 :
-  (index <=  41) ? M3 :
-  (index <=  42) ? M3 :
-  (index <=  43) ? M1 :
-  (index <=  47) ? L7 :
-  (index <=  49) ? M6 :
-  (index <=  51) ? M7 :
-  (index <=  53) ? H1 :
-  (index <=  55) ? M7 :
-  (index <=  57) ? M6 :
-  (index <=  58) ? M7 :
-  (index <=  59) ? M6 :
-  (index <=  63) ? M4 :
-  (index <=  65) ? M3 :
-  (index <=  67) ? M1 :
-  (index <=  69) ? M3 :
-  (index <=  71) ? M4 :
-  (index <=  73) ? M3 :
-  (index <=  74) ? M3 :
-  (index <=  75) ? M1 :
-  (index <=  79) ? L7 :
-  (index <=  81) ? M6 :
-  (index <=  83) ? M6 :
-  (index <=  87) ? M7 :
-  (index <=  89) ? M6 :
-  (index <=  91) ? M6 :
-  (index <=  95) ? M7 :
-  (index <=  97) ? M3 :
-  (index <=  99) ? M4 :
-  (index <= 100) ? M7 :
-  (index <= 101) ? M6 :
-  (index <= 103) ? M4 :
-  (index <= 109) ? M3 : 0;
+always @(posedge tempo) begin
+  state = state + 1;
+  case (index)
+    0,1: div = M6;
+    2,3: div = M6;
+    4,5,6,7: div = M7;
+    8,9: div = M6;
+    10,11: div = M6;
+    12,13,14,15: div = M7;
+    16,17: div = M6;
+    18,19: div = M7;
+    20,21: div = H1;
+    22,23: div = M7;
+    24,25: div = M6;
+    26: div = M7;
+    27: div = M6;
+    28,29,30,31: div = M4;
+    32,33: div = M3;
+    34,35: div = M1;
+    36,37: div = M3;
+    38,39: div = M4;
+    40,41: div = M3;
+    42: div = M3;
+    43: div = M1;
+    44,45,46,47: div = L7;
+    48,49: div = M6;
+    50,51: div = M7;
+    52,53: div = H1;
+    54,55: div = M7;
+    56,57: div = M6;
+    58: div = M7;
+    59: div = M6;
+    60,61,62,63: div = M4;
+    64,65: div = M3;
+    66,67: div = M1;
+    68,69: div = M3;
+    70,71: div = M4;
+    72,73: div = M3;
+    74: div = M3;
+    75: div = M1;
+    76,77,78,79: div = L7;
+    80,81: div = M6;
+    82,83: div = M6;
+    84,85,86,87: div = M7;
+    88,89: div = M6;
+    90,91: div = M6;
+    92,93,94,95: div = M7;
+    96,97: div = M3;
+    98,99: div = M4;
+    100: div = M7;
+    101: div = M6;
+    102,103: div = M4;
+    104,105,106,107,108,109: div = M3;
+    default: div = 0;
+  endcase
+end
 
 assign enable = (index <= 109);
 
