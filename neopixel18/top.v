8fca2798d22a71de192b400f47bf6daddf870521
Junxiao Shi <git@mail1.yoursunny.com>
2025-01-05 12:34:12 -0500

    neopixel18: write comments

--- neopixel18/top.v
+++ neopixel18/top.v
@@ -8,11 +8,6 @@ module top(
 inout PIN7, PIN8, PIN9, PIN10, PIN11, PIN12, PIN13, PIN14, PIN17, PIN18, PIN19, PIN20;
 output LEDn;
 
-// internal oscillator
-wire INTERNAL_OSC;
-defparam OSCH_inst.NOM_FREQ = "20.46";
-OSCH OSCH_inst(.STDBY(1'b0), .OSC(INTERNAL_OSC), .SEDSTDBY());
-
 // default pin state
 assign PIN7 = 0;
 assign PIN8 = 0;
@@ -26,28 +21,19 @@ assign PIN19 = 0;
 assign PIN20 = 0;
 assign LEDn = 0;
 
-// wires
-wire clk, rst, efbOvf, neoPixelPin, rdNext, msgTyp;
-reg [0:7] cnt;
-reg change;
-reg [143:0] colors;
-reg [5:0] index;
-wire [4:0] pos = index[5:1];
-wire ticker = index[0];
+// internal oscillator
+wire INTERNAL_OSC;
+defparam OSCH_inst.NOM_FREQ = "20.46";
+OSCH OSCH_inst(.STDBY(1'b0), .OSC(INTERNAL_OSC), .SEDSTDBY());
 
+// clock and reset
+wire clk, rst;
 assign clk = INTERNAL_OSC;
 assign rst = PIN10;
-assign PIN14 = neoPixelPin;
-
-// EFB counter
-efb EFB_inst(
-  .tc_clki(clk),
-  .tc_rstn(rst),
-  .tc_int(efbOvf),
-  .tc_oc()
-);
 
 // NeoPixel transmitter
+wire neoPixelPin, rdNext, msgTyp;
+assign PIN14 = neoPixelPin;
 neopixel_tx_fsm np(
   .clk(clk),
   .rst(rst),
@@ -60,7 +46,18 @@ neopixel_tx_fsm np(
   .neo_tx_out(neoPixelPin)
 );
 
+// EFB counter
+wire efbOvf; // overflow at 39960, approximately 512Hz
+efb EFB_inst(.tc_clki(clk), .tc_rstn(rst), .tc_int(efbOvf), .tc_oc());
+
 // color control
+reg [0:8] cnt; // counter for 512Hz to 1Hz
+reg change; // color rotation requested, set every 1Hz, clear upon completion
+reg [143:0] colors; // current colors
+reg [5:0] index;
+wire [4:0] pos = index[5:1]; // pixel position; reset WS2812 when >= pixel count
+wire ticker = index[0]; // transmitter needs two clock cycles per message
+
 assign msgTyp = pos < 5'd18;
 always @(posedge clk or negedge rst) begin
   if (!rst) begin
@@ -79,7 +76,7 @@ always @(posedge clk or negedge rst) begin
   end else begin
     if (efbOvf) begin
       cnt <= cnt + 1'b1;
-      if (cnt == 0) begin
+      if (cnt == 1'b0) begin
         change <= 1'b1;
       end
     end
